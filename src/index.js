ac82f3ed4df44040b1de619f2f730eaeee120166
Anders Kindall <akindall47@gmail.com>
2022-04-26 10:38:35 -0700

    Add data and test data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { select, arc } from "d3";
+import { select, json } from "d3";
 
 window.addEventListener('DOMContentLoaded', (e) => {
     console.log("try again");
-    
+    json('./data/single_family_home_by_state_array.json').then(data => {
+        console.log(data);
+    });
 });
 
