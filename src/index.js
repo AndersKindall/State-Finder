0c745b393fd4a257485aba0ce816bb6bfd203748
Anders Kindall <akindall47@gmail.com>
2022-04-26 11:57:51 -0700

    Update data json files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import { select, json } from "d3";
 
 window.addEventListener('DOMContentLoaded', (e) => {
     console.log("try again");
-    json('./data/single_family_home_by_state_array.json').then(data => {
+    json('./data/single_family_home_by_state_hash.json').then(data => {
         console.log(data);
+        console.log(Object.entries(data));
     });
 });
 
