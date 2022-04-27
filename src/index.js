ba38bd18056eb6da36aecb3403384e845e6005d4
Anders Kindall <akindall47@gmail.com>
2022-04-26 17:32:41 -0700

    Add h1 element and intial css stylings

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { select, json } from "d3";
 window.addEventListener('DOMContentLoaded', (e) => {
     console.log("try again");
     json('./data/single_family_home_by_state_array.json').then(data => {
-        // console.log(data);
         const california = data[0];
         const vals = Object.values(california);
         console.log(vals);
