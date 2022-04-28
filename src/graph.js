b034be6c7023677b712834c0ffddee281e3cdfb6
Anders Kindall <akindall47@gmail.com>
2022-04-28 09:47:01 -0700

    Add xAxis and yAxis

--- src/graph.js
+++ src/graph.js
@@ -1,4 +1,4 @@
-import { select, scaleLinear, max, min, csv, scaleBand, axisLeft } from "d3"
+import { select, scaleLinear, max, min, csv, scaleBand, axisLeft, axisBottom } from "d3"
 
 export const renderGraph = () => {
     csv('./data/data.csv').then(data => {
@@ -7,7 +7,7 @@ export const renderGraph = () => {
         const svg = select('svg');
         const width = +svg.attr('width');
         const height = +svg.attr('height');
-        const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+        const margin = { top: 20, right: 20, bottom: 20, left: 50};
         const innerWidth = width - margin.left - margin.right;
         const innerHeight = height - margin.top - margin.bottom;
     
@@ -15,20 +15,32 @@ export const renderGraph = () => {
             .domain([min(data, xValue), max(data, xValue)])
             .range([0, innerWidth]);
 
-        const xAsis = axisLeft(xScale);
+            
         const yScale = scaleBand()
             .domain(data.map(yValue))
-            .range([0, innerHeight]);
+            .range([0, innerHeight])
+            .padding(0.05);
 
         const g = svg.append('g')
-            .attr('transform', `translate${margin.left},${margin.top})`);
+            .attr('transform', `translate(${margin.left},${margin})`);
         
+        g.append('g').call(axisLeft(yScale));
+        g.append('g').call(axisBottom(xScale))
+            .attr('transform', `translate(0 ,${innerHeight})`);
+
         svg.selectAll('rect').data(data)
             .enter().append('rect')
                 .attr('y', d => yScale(yValue(d)))
                 .attr('width', d => xScale(xValue(d)))
                 .attr('height', yScale.bandwidth());
         
+        // svg.append('text')
+        //     .attr('class', 'yLabel')
+        //     .attr('x', -(height/2)-margin)
+        //     .attr('y', margin/2 - 20)
+        //     .attr('transform', 'rotate(-90)')
+        //     .attr('text-anchor', 'middle')
+        //     .text('Years')
     });
 }
 // let state = data[0];
